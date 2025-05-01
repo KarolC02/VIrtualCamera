eb7286be51290c759b203d1e359dc50b81d3793a
KarolAleksanderCichor <01178825@pw.edu.pl>
2025-05-01 20:19:51 +0200

    Add 27 cubes

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,9 @@ int main(int argc, char* argv[]) {
 
     float size = 10.0f;
 
-    for (int i = 0; i < 1; i++) {
-        for (int j = 0; j < 1; j++) {
-            for (int k = 0; k < 1; k++) {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            for (int k = 0; k < 3; k++) {
                 float x = 30.0f * i;
                 float y = 30.0f * j;
                 float z = 100.0f + 30.0f * k;
